a2cc1eb2c898cc9f2f88e22e7e5e48b6d80506cc
UsernameError3 <usernameerror3@outlook.com>
2023-06-12 07:01:07 -0500

    established dynamic rendering

---
+++ assets/js/twitchEmbed.js
@@ -0,0 +1,39 @@
+function runTwitchEmbed(Twitch) {
+
+    function initiate() {
+        player.addEventListener(Twitch.Player.ONLINE, handleOnline);
+        player.addEventListener(Twitch.Player.OFFLINE, handleOffline);
+        player.removeEventListener(Twitch.Player.READY, initiate);
+    }
+    
+    function handleOnline() {
+        document.getElementById("twitch").classList.remove('hide');
+        player.removeEventListener(Twitch.Player.ONLINE, handleOnline);
+        player.addEventListener(Twitch.Player.OFFLINE, handleOffline);
+        player.setMuted(false);
+    }
+    
+    function handleOffline() {
+        document.getElementById("twitch").classList.add('hide');
+        player.removeEventListener(Twitch.Player.OFFLINE, handleOffline);
+        player.addEventListener(Twitch.Player.ONLINE, handleOnline);
+        player.setMuted(true);
+    }
+
+    var options = {
+        channel: "velyna",
+        width: 640,
+        height: 360,
+        parent: ['localhost', 'velyna.net']
+    };
+
+    try {
+        var player = new Twitch.Player("twitch", options);
+        player.addEventListener(Twitch.Player.READY, initiate);
+        
+    } catch (error) {
+        console.log('Twitch Error: ', error);
+    }
+};
+
+export { runTwitchEmbed };
